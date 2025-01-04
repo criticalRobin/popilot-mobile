250b24e1e78ee27d1d42f27d1edb7b338749a4de
criticalRobin <matiastite1@gmail.com>
2025-01-04 13:19:39 -0500

    Implementado appbar de la aplicacion

---
+++ lib/shared/appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:popilot_mobile/providers/auth_provider.dart';
+import 'package:popilot_mobile/utils/colors.dart';
+
+class PopilotAppbar extends ConsumerStatefulWidget {
+  const PopilotAppbar({super.key});
+
+  @override
+  ConsumerState<PopilotAppbar> createState() => _PopilotAppbarState();
+}
+
+class _PopilotAppbarState extends ConsumerState<PopilotAppbar> {
+  @override
+  Widget build(BuildContext context) {
+    final authNotifier = ref.read(authProvider.notifier);
+    final authState = ref.watch(authProvider);
+
+    return Row(
+      children: [
+        Image(
+          image: AssetImage('assets/images/popilot.webp'),
+          fit: BoxFit.cover,
+          height: 45,
+          width: 125,
+        ),
+        const Spacer(),
+        Text(authState.user?.username ?? 'usuario'),
+        IconButton(
+          icon: Icon(
+            Icons.logout,
+            color: AppColors.white,
+          ),
+          onPressed: () async {
+            await authNotifier.signOut();
+            context.go('/');
+          },
+        ),
+      ],
+    );
+  }
+}
